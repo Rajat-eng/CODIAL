c53381d3053bf77e8f466f9ed26e79a342bdb821
Rajat <vrajat269@gmail.com>
2022-06-15 22:16:09 +0530

    set up database and project set up is complete

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log("Cannot run database"));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/codeial_development');
+  console.log("Database successfully accessed");
+}
+
+
