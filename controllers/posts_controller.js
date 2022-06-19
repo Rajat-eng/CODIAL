e4a16c99c8af791f0e3a0c14234c9d32288b4110
Rajat <vrajat269@gmail.com>
2022-06-19 17:49:16 +0530

    added post comments and partials

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
+
 
 module.exports.create=function(req,res){
     Post.create({
@@ -8,4 +10,19 @@ module.exports.create=function(req,res){
         if(err){console.log("cannot create post"); return;}
         return res.redirect('back');
     });
+};
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        // ideally user.id should be user._id but user.id works bcoz it is converted into string by mongoose
+        //post.user is accessed in post schema which is userID and req.user.id
+        if(post.user==req.user.id){ // only user who has posted can delete it
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
 };
