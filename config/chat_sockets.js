e87f39d2cf649d6bf6330c5741b0c21e5b414e87
Rajat <vrajat269@gmail.com>
2022-07-23 16:31:14 +0530

    final

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -3,7 +3,7 @@
 module.exports.chatSockets=function(socketServer){
     let io=require('socket.io')(socketServer,{
         cors:{
-            origin: "http://localhost:8000",
+            origin: "*",
             methods: ["GET", "POST"],
             transports:["websockets","polling"],
             credentials: true
