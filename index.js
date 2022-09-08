c312208ab9e3185672febd232156d641932321ff
Rajat Verma <96742104+Rajat-eng@users.noreply.github.com>
2022-09-09 00:11:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const express= require('express');
 const app=express();
 require('./config/view_helpers')(app);
 
-
-// environment
-const env=require('./config/environment');
-const logger=require('morgan');
-// cookies
-const cookieParser=require('cookie-parser');
-
 const port=8000;
 const db=require('./config/mongoose');
 const path=require('path');
 
+
+// environment
+const env=require('./config/environment');
+const logger=require('morgan');
+
+// parsers
+const cookieParser=require('cookie-parser');
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser('secret'));
+
+
+
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+// use static scss 
+app.use(express.static(path.join(__dirname,env.asset_path)));
+
 // layouts
 const expressLayouts=require('express-ejs-layouts');
+// use layouts
+app.use(expressLayouts);
+
+// extract style and scripts from subpages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // passport
 const passport=require('passport');
@@ -24,23 +42,11 @@ const passportGoogle=require('./config/passport-google-oauth2-strategy');
 const MongoStore=require('connect-mongo');
 
 
-// chat server
-const http = require('http');
-const chatServer = http.createServer(app);
-const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000, () => {
-  console.log('chat Server listening on :5000');
-});
-
-
-
-// session cookies
-const session=require('express-session'); // create session cookie in encrypred form
-
-// SASS
+// SASS and Flash Middleware
 const sassMiddleware=require('node-sass-middleware');
 const flash=require('connect-flash');
 const customMware=require('./config/middleware');
+
 
 if(env.name=='development'){
     app.use(sassMiddleware({
@@ -54,29 +60,12 @@ if(env.name=='development'){
 }
 
 
-// parser use
-app.use(express.urlencoded({extended:false}));
-app.use(cookieParser('secret'));
-
-// use layouts
-app.use(expressLayouts);
-
-
-// extract style and scripts from subpages into the layout
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
-
-
-// use static
-app.use(express.static(env.asset_path));
-
 // make uploads path available 
+console.log(__dirname);
 app.use('/uploads',express.static( path.join(__dirname,'uploads')) );
 
-// set up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
-
+// session cookies
+const session=require('express-session'); // create session cookie in encrypred form
 //middleware for session
 // mongo store is used to store session db
 app.use(session({
@@ -90,7 +79,7 @@ app.use(session({
         },
         store:MongoStore.create( // session gets permanenntly stored even if server expires
             {
-                mongoUrl:'mongodb://localhost:27017/codeial_development',   
+                mongoUrl:`mongodb://localhost:27017/${env.db}`,   
                 autoRemove:'disabled'
             },
             function(err){
@@ -100,17 +89,28 @@ app.use(session({
     })
 );
 
+
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); 
 app.use(passport.setAuthenticatedUser); // checks if a cookie is present and sends user data to local
-app.use(flash());
+app.use(flash()); // this requires session
 app.use(customMware.setFlash);
 
 // use morgan
 app.use(logger(env.morgan.mode,env.morgan.options));
 
+
 //  use routes
 app.use('/',require('./routes'));
+
+
+// chat server
+const http = require('http');
+const chatServer = http.createServer(app);
+const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000, () => {
+  console.log('chat Server listening on :5000');
+});
 
 // running engine
 app.listen(port,function(err){
